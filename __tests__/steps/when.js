f5d259eab76bbe5bc23ab01533155b9b7e16ef18
Ezra Bowden <ezra@redmacaw.io>
2021-10-26 08:29:33 +0000

    Failing test

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -107,9 +107,46 @@ const a_user_calls_getMyProfile = async (user) => {
   return profile;
 };
 
+const a_user_calls_editMyProfile = async (user, input) => {
+  const editMyProfile = `mutation editMyProfile($input: ProfileInput!) {
+    editMyProfile(newProfile: $input) {
+      id
+      name
+      screenName
+      imageUrl
+      backgroundImageUrl
+      bio
+      location
+      website
+      birthdate
+      createdAt
+      followersCount
+      followingCount
+      tweetsCount
+      likesCount
+    }
+  }`;
+
+  const variables = {
+    input,
+  };
+
+  const data = await GraphQL(
+    process.env.API_URL,
+    editMyProfile,
+    variables,
+    user.accessToken
+  );
+  const profile = data.editMyProfile;
+  console.log(`[${user.username}] - edited profile`);
+
+  return profile;
+};
+
 module.exports = {
   we_invoke_confirmUserSignup,
   a_user_signs_up,
   we_invoke_an_appsync_template,
   a_user_calls_getMyProfile,
+  a_user_calls_editMyProfile,
 };
