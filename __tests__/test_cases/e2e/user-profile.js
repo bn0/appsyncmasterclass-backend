4499d02ab9ed57c4cbfc77925762880ae830acf9
Ezra Bowden <ezra@redmacaw.io>
2021-11-02 03:39:05 +0000

    Add acceptance test for getImageUploadUrl query

--- __tests__/test_cases/e2e/user-profile.js
+++ __tests__/test_cases/e2e/user-profile.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const chance = require("chance").Chance();
+const path = require("path");
 
 const given = require("../../steps/given");
 const when = require("../../steps/when");
+const then = require("../../steps/then");
 
 describe("Given an authenticated user", () => {
   let user, profile;
@@ -34,6 +37,35 @@ describe("Given an authenticated user", () => {
     const [firstName, lastName] = profile.name.split(" ");
     expect(profile.screenName).toContain(firstName);
     expect(profile.screenName).toContain(lastName);
+  });
+
+  it("The user can get a URL to upload new profile image", async () => {
+    const extension = ".png";
+    const contentType = "image/png";
+    const uploadUrl = await when.a_user_calls_getImageUploadUrl(
+      user,
+      extension,
+      contentType
+    );
+
+    // Get upload URL
+    const bucketName = process.env.BUCKET_NAME;
+    const regex = new RegExp(
+      `https://${bucketName}.s3-accelerate.amazonaws.com/${user.username}/.*${
+        extension || ""
+      }\?.*Content-Type=${
+        contentType ? contentType.replace("/", "%2F") : "image%2Fjpeg"
+      }.*`
+    );
+    expect(uploadUrl).toMatch(regex);
+
+    // Attempt to upload image
+    const filePath = path.join(__dirname, "../../data/logo.png");
+    await then.user_can_upload_image_to_url(uploadUrl, filePath, contentType);
+
+    // Attempt to download image
+    downloadUrl = uploadUrl.split("?")[0];
+    await then.user_can_download_image_from(downloadUrl);
   });
 
   it("The user can edit his profile with editMyProfile", async () => {
