58e4adf731967c32d15d5c43f3f928ed75ad826b
Ezra Bowden <ezra@redmacaw.io>
2021-11-07 23:17:35 +0000

    Add integration tests for tweet mutation

--- __tests__/steps/then.js
+++ __tests__/steps/then.js
@@ -19,6 +19,60 @@ const user_exists_in_UsersTable = async (id) => {
   return resp.Item;
 };
 
+const tweet_exists_in_TweetsTable = async (id) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+  console.log(
+    `Looking for tweet [${id}] in table [${process.env.TWEETS_TABLE}]`
+  );
+  const resp = await DynamoDB.get({
+    TableName: process.env.TWEETS_TABLE,
+    Key: {
+      id,
+    },
+  }).promise();
+
+  expect(resp.Item).toBeTruthy();
+
+  return resp.Item;
+};
+
+const tweet_exists_in_TimelinesTable = async (userId, tweetId) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+  console.log(
+    `Looking for tweet [${tweetId}] for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`
+  );
+  const resp = await DynamoDB.get({
+    TableName: process.env.TIMELINES_TABLE,
+    Key: {
+      userId,
+      tweetId,
+    },
+  }).promise();
+
+  expect(resp.Item).toBeTruthy();
+
+  return resp.Item;
+};
+
+const tweetsCount_is_updated_in_UsersTable = async (id, newCount) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+  console.log(`Looking for user [${id}] in table [${process.env.USERS_TABLE}]`);
+  const resp = await DynamoDB.get({
+    TableName: process.env.USERS_TABLE,
+    Key: {
+      id,
+    },
+  }).promise();
+
+  expect(resp.Item).toBeTruthy();
+  expect(resp.Item.tweetsCount).toEqual(newCount);
+
+  return resp.Item;
+};
+
 const user_can_upload_image_to_url = async (url, filepath, contentType) => {
   const data = fs.readFileSync(filepath);
   await http({
@@ -47,4 +101,7 @@ module.exports = {
   user_exists_in_UsersTable,
   user_can_upload_image_to_url,
   user_can_download_image_from,
+  tweet_exists_in_TweetsTable,
+  tweet_exists_in_TimelinesTable,
+  tweetsCount_is_updated_in_UsersTable,
 };
