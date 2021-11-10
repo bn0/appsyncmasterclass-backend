df000333f7fe955cad20da52b9018f5e5052fd3a
Ezra Bowden <ezra@redmacaw.io>
2021-11-10 02:33:08 +0000

    Remove unnecessary DynamoDB calls

--- __tests__/steps/given.js
+++ __tests__/steps/given.js
@@ -22,7 +22,7 @@ const a_random_user = () => {
   };
 };
 
-const an_appsync_context = (identity, args, result, source) => {
+const an_appsync_context = (identity, args, result, source, info) => {
   const util = velocityUtil.create([], new Date(), Object());
   const context = {
     identity,
@@ -30,6 +30,7 @@ const an_appsync_context = (identity, args, result, source) => {
     arguments: args,
     result,
     source,
+    info,
   };
   return {
     context,
